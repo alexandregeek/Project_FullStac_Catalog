9dc5329a9756134df1d18ce495c4dc34c5e2294f
alexandregeek <alexandregeek@hotmail.com>
2021-10-31 16:27:43 -0300

    Extensão Prettier - Code formatter

--- frontweb/src/App.js
+++ frontweb/src/App.js
@@ -1,9 +1,9 @@
 import './App.css';
-
-function App() {
+import'./assets/styles/custom.scss'
+            function App() {
   return (
     <h1>Hello DSCatalog</h1>
-  );
+                  );
 }
 
 export default App;
