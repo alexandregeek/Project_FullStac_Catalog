c7c1064c599df9e3b6a4753c9af1a2c7c3428168
alexandregeek <alexandregeek@hotmail.com>
2021-10-31 18:12:50 -0300

    confirmar

--- frontweb/src/App.js
+++ frontweb/src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
-import'./assets/styles/custom.scss'
-            function App() {
+import './assets/styles/custom.scss'
+function App() {
   return (
     <h1>Hello DSCatalog</h1>
-                  );
+  );
 }
 
 export default App;
+//Git testes 
